2205484d9507b6fbc23821304d3f79c74e3ddaef
Damien Millot <Damien.Millot@diiage.org>
2019-12-27 18:37:59 +0100

    fix book model (unsaved)

--- BookPOC/Models/Book.cs
+++ BookPOC/Models/Book.cs
@@ -7,5 +7,22 @@ namespace BookPOC.Models
 {
     public class Book
     {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public string LongDescription { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public bool InStock { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
     }
 }
