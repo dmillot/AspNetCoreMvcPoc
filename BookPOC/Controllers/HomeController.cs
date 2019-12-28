2e1f7411223519d2865015b71ce4a03cec81f861
Damien Millot <Damien.Millot@diiage.org>
2019-12-28 10:54:02 +0100

    Create HomeController

---
+++ BookPOC/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookPOC.Repository;
+using BookPOC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookPOC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public HomeController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+        public IActionResult Index()
+        {
+            var listBook = new BookListViewModel
+            {
+                Books = _bookRepository.GetAllBooks().Take(3)
+            };
+
+            return View(listBook);
+        }
+    }
+}
