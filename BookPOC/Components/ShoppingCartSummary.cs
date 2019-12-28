587bc12afbd08349835e12aa40942e8f8da29a72
Damien Millot <Damien.Millot@diiage.org>
2019-12-28 18:09:05 +0100

    add fontawesome lib and create view component for shopping cart

---
+++ BookPOC/Components/ShoppingCartSummary.cs
@@ -0,0 +1,27 @@
+﻿using BookPOC.Models;
+using BookPOC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookPOC.Components
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            return this.View(shoppingCartViewModel);
+        }
+    }
+}
